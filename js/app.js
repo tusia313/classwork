4b6a099bf035fcc84bd06358d44805ddf5c26f72
Marta Łoskot <loskotmarta@gmail.com>
2023-11-07 13:33:11 +0100

    My solution of label/break task

--- js/app.js
+++ js/app.js
@@ -2,22 +2,38 @@ let round = 0
 let count = 0
 let punished = true
 
-loopPunishment: while (punished) {
+// loopPunishment: while (punished) {
+//     round++
+//     while (punished) {
+//         count++
+//         console.log(count + '. I will use camelCase, ' + round)
+
+//         if (round >= 4 && count >= 100) {
+//             console.log("Eat my shorts!")
+//             break loopPunishment
+
+//         } else if (count >= 100) {
+//             console.log('Round done!')
+//             count = 0
+//             break
+//         }
+//     }
+// }
+
+while (round < 4) {
     round++
-    while (punished) {
-        count++
-        console.log(count + '. I will use camelCase, ' + round)
-
-        if (round >= 4 && count >= 100) {
-            console.log("Eat my shorts!")
-            break loopPunishment
-
-        } else if (count >= 100) {
-            console.log('Round done!')
-            count = 0
-            break
-        }
+    for (count = 0; count < 100; count++) {
+        console.log(count + 1 + '. I will use camelCase', round)
     }
+
+    if (count === 100 && round === 4) {
+        console.log('Eat my shorts!')
+    }
+
+    else if (count === 100) {
+        console.log('Round done')
+    }
+
 }
 
 
