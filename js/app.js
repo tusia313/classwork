cd1ab196f2ffd3b497b76cc3e20112b7aa60a71f
Marta Łoskot <loskotmarta@gmail.com>
2023-11-07 11:44:43 +0100

    Add basic files.

---
+++ js/app.js
@@ -0,0 +1,30 @@
+let bitcoin = 1500230008769
+
+const trillion = 10 ** 12
+const million = 10 ** 6
+
+console.log(million)
+
+if (bitcoin > trillion) {
+    console.log("You are a trionell")
+}
+
+else if (bitcoin > million){
+    console.log("You are a milioner !")
+}
+
+else {
+    console.log("You poor soul...")
+}
+
+
+
+
+
+
+
+
+
+
+
+
