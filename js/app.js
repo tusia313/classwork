ed13c59132ca2fb7f985b4e1f4e6949009c942dd
Marta Łoskot <loskotmarta@gmail.com>
2023-11-08 12:56:29 +0100

    Practicing new Date() method

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 
-const randomInteger = () => Math.floor(Math.random() * 100) 
 
-console.log(randomInteger())
+function dayPasses() {
+    const christmas = new Date("December 24, 2022")
+    const today = new Date()
+    return (today - christmas)/1000/60/60/24
+}
 
-const getCircus = radious => Math.PI * 2 * radious
-
-console.log(getCircus(4.5))
+console.log(dayPasses())
 
 
 
