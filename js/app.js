360c9eb5bc44472dd61014fb8d2c6bbd858c28e6
Marta Łoskot <loskotmarta@gmail.com>
2024-01-15 13:20:45 +0100

    fixing second task of number objects

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-const netflix2022 = 210.32344
-const netflix2021 = 192.4392
+// const netflix2022 = 210.32344
+// const netflix2021 = 192.4392
 
-function seeTwoNumber () {
-    const total = netflix2022 + netflix2021
-    return Number(total.toFixed(2))
+// function seeTwoNumber () {
+//     const total = netflix2022 + netflix2021
+//     return Number(total.toFixed(2))
+// }
+
+// console.log(seeTwoNumber())
+
+const meal = 43.23
+const drinks = 21.32
+
+function bill () {
+    const total = meal + drinks
+    return (Number(total) + (Number(total)*0.12)).toFixed(2)
 }
 
-console.log(seeTwoNumber())
-
+console.log(bill())
 
 
 
