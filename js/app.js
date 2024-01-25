7af152eb46aa5d0fd161f677ea0fde268304061d
Marta Łoskot <loskotmarta@gmail.com>
2024-01-25 12:05:10 +0100

    Starting fridge exercise

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
-const sheep = document.querySelectorAll('.sheep')
-
-    const numberOfSheep = sheep.length
-    console.log(numberOfSheep)
+// .shift() exercise
+// Write a function that will remove the first element from the
+// top shelf in the fridge every time we click on the fridge.
+// Hint: use the following methods and properties:
+// .querySelector(), .innerHTML, .shift(), .addEventListener()
+const fridge = document.querySelector(".fridge")
+const topShelf = document.querySelector("#top-shelf")
+const foodItems = ["🍉", "🍊", "🍇", "🍌", "🍍"]
