fc547904f3219b400b9102482226ca3e029c25d0
Marta Łoskot <loskotmarta@gmail.com>
2024-01-25 14:21:06 +0100

    Adding new task - sorting numbers

--- js/app.js
+++ js/app.js
@@ -1,15 +1,7 @@
-// .shift() exercise
-// Write a function that will remove the first element from the
-// top shelf in the fridge every time we click on the fridge.
-// Hint: use the following methods and properties:
-// .querySelector(), .innerHTML, .shift(), .addEventListener()
-const fridge = document.querySelector(".fridge")
-const topShelf = document.querySelector("#top-shelf")
-const foodItems = ["🍉", "🍊", "🍇", "🍌", "🍍"]
-topShelf.innerHTML = foodItems
 
-function removeItem(){
-    foodItems.shift()
-    topShelf.innerHTML = foodItems
-}
-fridge.addEventListener('click', removeItem)
+// .sort() exercise
+// Sort the orders by ascending order and show them in the
+// order display.
+//Hint: Use sort(), querySelector(), .innerHTML
+const displayElement = document.querySelector(".display")
+const orders = [453, 124, 402, 99]
