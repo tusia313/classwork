eb8a9cdcc362268f00579f00f64882bbe4137e1b
Marta Łoskot <loskotmarta@gmail.com>
2024-02-05 17:36:50 +0100

    Arrays task

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
-// .push()
-// You are the owner of a farm and you have just acquired a new
-// farm animal 🐓! Add your farm animal to the pack by pressing on
-// the button.
-// Hint: use .querySelector() .push() .addEventListener()
-const farm = document.querySelector(".farm")
-const buttonElement = document.querySelector("button")
-const animals = ["🐷", "🐮", "🐴", "🐑"]
-farm.innerHTML = animals.join(" ")
-const animal = "🐓"
+// 1. Join these two arrays together and then sort the words by
+// longest to shortest.
+const primaryColors = ["red", "blue", "yellow"]
+const pastelColors = ["lilac", "beige", "azure"]
 
-function addKogut() {
-    animals.push(animal)
-    farm.innerHTML = animals.join(" ")
-}
-buttonElement.addEventListener('click', addKogut)
+const allColors = primaryColors.concat(pastelColors)
+console.log(allColors)
+console.log(allColors.sort((a,b) => b.length - a.length))
+
+// 2. Put the numbers in order and then add the missing ones.
+const count = [1, 0, 8, 10, 7, 3, 9, 2]
+
+console.log(count.sort((a,b) => a - b))
+count.splice(4, 0, 4, 5, 6)
+console.log(count)
+
+// 3. Make a sentence out of this array without the last word.
+const words = ["The", "cat", "jumped", "over", "the", "moon", "if"]
+
+words.pop()
+console.log(words.join(' '))
+
+
+// 4. Sort these items in order and remove the odd one out.
+const years = ["bed", 1990, 1734, 2022, 1634]
+
+console.log(years.sort((a,b) => a -b))
+console.log(years.splice(0, 1))
+console.log(years)
