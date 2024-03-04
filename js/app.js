1f5555f73cf12d19ac74e2b1c4ba9e39d8e9da84
Marta Łoskot <loskotmarta@gmail.com>
2024-03-04 22:07:08 +0100

    Add new exercise

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
-const vintedBrands = ['Zara', 'Topshop', 'Balagan', 'Bohemia Souls']
+// Array exercise
 
-const serachedBrand = vintedBrands.some(vintedBrand => vintedBrand === 'Balagan')
-console.log(serachedBrand)
+// 1. Write function that will change the temperatures from Celsius
+// to Fahrenheit.
+// eg: getCelsius([100, 5, 25, 10]) returns [212, 41, 77, 50]
+// (num x 9/5) + 32 = F
+const degrees = [100, 5, 25, 10]
+function getCelsius() {
+  return degrees.map(degree => (degree * 9/5) + 32)
+}
+console.log(getCelsius(degrees))
 
-const zaraDress = ['long dress', 'long dress', 'long dress']
-const newDress = zaraDress.map(dress => dress === 'dress with white tail')
-console.log(newDress)
+// 2. Using '.join()' and 'length' write a function that will return
+// the total number of characters in an array of strings.
+// eg: getTotal(['Mirror', 'Balloon', 'Ox']) returns 15
 
-const gameTotals = ['110', '23', '67']
+// 3. Using '.some()' write a function that will check an array
+// for falsy values.
+// eg: checkForFalsy([null, 0, [], -3]) returns true
 
-
-
-console.log('hello vscCode')
+// 4. Using '.split()', '.filter()', '.includes' and '.length()'
+// write a function that will tell you how many vowels there are
+// in a sentence.
+// eg: vowelCount('I am a little sausage') returns 9.
