c9369bbb2428da2f233520358ce8f9481183ac42
Marta Łoskot <loskotmarta@gmail.com>
2024-03-05 21:59:34 +0100

    Objects exercises

--- js/app.js
+++ js/app.js
@@ -1,44 +1,39 @@
-// Array exercise
+const restaurants = [
+  {
+    name: "Tony's Pizzera",
+    cuisine: "Italian",
+    rating: 4,
+    address: {
+      first_line: "45 Lombard Street",
+      second_line: "London",
+      postcode: "SE1 4DF",
+      contacts: ["07732102351", "07839375921"]
+    }
+  },
+  {
+    name: "Sushi Swish",
+    cuisine: "Japanese",
+    rating: 5,
+    address: {
+      "First Line": "21 Avington Lane",
+      "Second Line": "London",
+      Postcode: "SW1 4PE",
+      contacts: ["07729473621", "07734967261"]
+    }
+  }
+]
 
-// 1. Write function that will change the temperatures from Celsius
-// to Fahrenheit.
-// eg: getCelsius([100, 5, 25, 10]) returns [212, 41, 77, 50]
-// (num x 9/5) + 32 = F
-const degrees = [100, 5, 25, 10]
-function getCelsius() {
-  return degrees.map(degree => (degree * 9/5) + 32)
-}
-console.log(getCelsius(degrees))
+// 1. Get Sushi Swishes cuisine
+console.log(restaurants[1]['cuisine'])
+// 2. Get Tony's Pizzera's postcode
+console.log(restaurants[0]['address']['postcode'])
+// 3. Get both the restaurants names
+restaurants.forEach(restaurant => console.log(restaurant.name))
+// 4. Get Tony's Pizzera's first line of their address
+console.log(restaurants[0].address.first_line)
+// 5. Get Sushi Swishes first line of their address
+console.log(restaurants[1].address["First Line"])
+// 6. Get Sushi Swishes second contact number
+console.log(restaurants[1].address.contacts[1])
 
-// 2. Using '.join()' and 'length' write a function that will return
-// the total number of characters in an array of strings.
-// eg: getTotal(['Mirror', 'Balloon', 'Ox']) returns 15
-const words = ['Mirror', 'Balloon', 'Ox']
-function getNumber() {
-  const newWord = words.join('').length
-  return newWord
-}
-console.log(getNumber(words))
 
-// 3. Using '.some()' write a function that will check an array
-// for falsy values.
-// eg: checkForFalsy([null, 0, [], -3]) returns true
-const values = [null, 0, [], -3]
-function getTrue() {
-
-  return values.some(value => value = true)
-}
-console.log(getTrue())
-
-// 4. Using '.split()', '.filter()', '.includes' and '.length()'
-// write a function that will tell you how many vowels there are
-// in a sentence.
-// eg: vowelCount('I am a little sausage') returns 9.
-const sentence ='I am a little sausage'
-
-function howMany() {
-  const volwes = ['a', 'e', 'i', 'o', 'u', 'y']
-  const letters = sentence.toLowerCase().split('')
-  return letters.filter(letter => volwes.includes(letter)).length
-}
-console.log(howMany())
