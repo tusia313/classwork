008522d85ffdabdd97d70ddb255001536d5f68f4
Marta Łoskot <loskotmarta@gmail.com>
2024-03-22 17:02:16 +0100

    Select dropdown in JS

--- js/app.js
+++ js/app.js
@@ -1,27 +1,14 @@
-// setInterval() exercise
-// Make a countdown timer that will show you the time every
-// 1000ms, and stop when we hit zero OR press the button.
-const counter = document.querySelector('h1')
-const button = document.querySelector('button')
+const selectDrop = document.querySelector('#flavor-select')
+const submitDrop = document.querySelector('input[type=submit]')
+
+console.log(selectDrop)
+console.log(submitDrop)
 
 
-let counting = 10
-counter.innerText = counting
-let timeId
 
-function startCounting() {
-  if (counting > 0) {
-    counting--
-    counter.innerText = counting
-  } 
-  else {
-    clearInterval(timeId)
-  }
-}
-function stopCounting() {
-  clearInterval(timeId)
+function handleClick(e) {
+  e.preventDefault()
+  console.log(selectDrop.value)
 }
 
-timeId = setInterval(startCounting, 1000)
-button.addEventListener('click', stopCounting)
-
+submitDrop.addEventListener('click', handleClick)
