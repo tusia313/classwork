1e3219a6f827e579188acfaaf5052c9653964204
Marta Łoskot <loskotmarta@gmail.com>
2024-04-23 12:44:30 +0200

    POST request

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 // GET HTTP request method
-fetch('https://my-burger-api.herokuapp.com/burgers/9')
+const data = {
+    name: 'Martha Beskid Burger'
+}
+fetch('https://my-burger-api.herokuapp.com/burgers', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+})
     .then(response => response.json())
     .then(data => console.log(data))
