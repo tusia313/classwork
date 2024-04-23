8a14c88cf0216b2d4f1052f10dc8b91e6d3eb0fc
Marta Łoskot <loskotmarta@gmail.com>
2024-04-23 18:13:21 +0200

    put http request

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 // POST HTTP request method
+// const data = {
+//     name: 'Martha Beskid Burger'
+// }
+// fetch('https://my-burger-api.herokuapp.com/burgers', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+// })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+// PUT http request
 const data = {
-    name: 'Martha Beskid Burger'
+    name: 'Martha Beskid Burger',
+    restaurant: 'Poland',
+    ingredients: ['becon', 'corn', 'onion']
 }
-fetch('https://my-burger-api.herokuapp.com/burgers', {
-    method: 'POST',
+fetch('https://my-burger-api.herokuapp.com/burgers/28', {
+    method: 'PUT',
     headers: {
         'Content-Type': 'application/json'
     },
@@ -11,3 +27,4 @@ fetch('https://my-burger-api.herokuapp.com/burgers', {
 })
     .then(response => response.json())
     .then(data => console.log(data))
+    .catch(error => console.error(error))
