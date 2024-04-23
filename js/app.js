bf5c9888d72f5db88dede2f013d4858171c0dea9
Marta Łoskot <loskotmarta@gmail.com>
2024-04-23 11:37:06 +0200

    get http request method

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-const containerElement = document.querySelector("#container")
-const imageElement = document.createElement("img")
-
-imageElement.setAttribute('src','https://storage.googleapis.com/pod_public/1300/151089.jpg')
-imageElement.setAttribute('alt', 'Picture of a cat')
-imageElement.setAttribute('width', '10rem')
-
-containerElement.append(imageElement)
+// GET HTTP request method
+fetch('https://my-burger-api.herokuapp.com/burgers/9')
+    .then(response => response.json())
+    .then(data => console.log(data))
