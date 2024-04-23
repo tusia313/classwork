e74039052c2c7105176c7184f0d139f2b328a11e
Marta Łoskot <loskotmarta@gmail.com>
2024-04-23 18:31:01 +0200

    delete request

--- js/app.js
+++ js/app.js
@@ -13,18 +13,26 @@
 //     .then(data => console.log(data))
 
 // PUT http request
-const data = {
-    name: 'Martha Beskid Burger',
-    restaurant: 'Poland',
-    ingredients: ['becon', 'corn', 'onion']
-}
+// const data = {
+//     name: 'Martha Beskid Burger',
+//     restaurant: 'Poland',
+//     ingredients: ['becon', 'corn', 'onion']
+// }
+// fetch('https://my-burger-api.herokuapp.com/burgers/28', {
+//     method: 'PUT',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+// })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error))
+
+// Delete !
+
 fetch('https://my-burger-api.herokuapp.com/burgers/28', {
-    method: 'PUT',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
+    method: 'DELETE'
 })
     .then(response => response.json())
     .then(data => console.log(data))
-    .catch(error => console.error(error))
