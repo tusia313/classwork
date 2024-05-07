a25754a0a5fac7ba4d4cd03ac255b060dcda9a26
Marta Łoskot <loskotmarta@gmail.com>
2024-05-07 16:36:19 +0200

    Await and async exercise

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-const print = async () => 'I have beend printed'
+// const print = async () => 'I have beend printed'
 
-print().then(response => console.log(response))
+// print().then(response => console.log(response))
 
 // fetch('https://restcountries.com/v3.1/all')
+
+async function getData() {
+    const response = await fetch('https://restcountries.com/v3.1/all')
+    const data = await response.json()
+    console.log(data)
+}
+
+getData()
