a1de5c113a878968233311468f2ae7597b4aa771
Marta Łoskot <loskotmarta@gmail.com>
2024-05-25 21:16:48 +0200

    Add data from browser

--- server/index.js
+++ server/index.js
@@ -3,40 +3,33 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('hello world !!!')
-})
+const url = "https://22a8e260-1c21-4d00-a294-1e3a98d82d95-europe-west2.apps.astra.datastax.com/api/rest/v2/namespaces/database_section/collections/users"
+const token = "AstraCS:OXScQTBdFpTrNbRMvPHRCyPO:c05aa1f91bdaf3b2594724d586d36a497101ce85f20ce5d2a07d8b3c912b7a56"
 
-app.get('/users', async (req, res) => {
+app.post('/users', async (req, res) => {
+    console.log(req)
     const options = {
+        method: "POST",
         headers: {
-            "X-Cassandra-Token": token
+            accept: "application/json",
+            'X-Cassandra-Token': token,
+            "Content-Type": "application/json"
+        },
+        data: {
+            firstName, lastName
         }
     }
     try {
-        const response = await axios(`${url}/?page-size=20`, options)
+        const response = await axios(url, options)
         res.send(response.data)
     } catch (err) {
         console.error(err)
     }
 })
 
-app.get('/users/:documentId', async (req, res) => {
-    console.log(req.params.documentId)
-    const id = req.params.documentId
-    const options = {
-        headers: {
-            "X-Cassandra-Token": token
-        }
-    }
-    try {
-        const response = await axios(`${url}/${id}`, options)
-        res.send(response.data)
-    } catch (err) {
-        console.error(err)
-    }
-})
+
 
 app.listen(PORT, () => console.log('listening out to port ' + PORT))
 
