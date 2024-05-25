e35a6512fbe78725827502122fcf2f50d2b6e949
Marta Łoskot <loskotmarta@gmail.com>
2024-05-25 16:58:52 +0200

    Axios method

--- server/index.js
+++ server/index.js
@@ -1,18 +1,41 @@
 const PORT = 8000
 const express = require('express')
 const app = express()
+const axios = require('axios')
+
 
 app.get('/', (req, res) => {
     res.send('hello world !!!')
 })
 
-app.get('/users', (req, res) => {
-    res.send('all my users')
+app.get('/users', async (req, res) => {
+    const options = {
+        headers: {
+            "X-Cassandra-Token": token
+        }
+    }
+    try {
+        const response = await axios(`${url}/?page-size=20`, options)
+        res.send(response.data)
+    } catch (err) {
+        console.error(err)
+    }
 })
 
-app.get('/users/:userId', (req, res) => {
-    console.log(req.params.userId)
-    res.send(req.params.userId)
+app.get('/users/:documentId', async (req, res) => {
+    console.log(req.params.documentId)
+    const id = req.params.documentId
+    const options = {
+        headers: {
+            "X-Cassandra-Token": token
+        }
+    }
+    try {
+        const response = await axios(`${url}/${id}`, options)
+        res.send(response.data)
+    } catch (err) {
+        console.error(err)
+    }
 })
 
 app.listen(PORT, () => console.log('listening out to port ' + PORT))
