a31652924882cd0a1b3d4d4a325b87eb10282cd0
Marta Łoskot <loskotmarta@gmail.com>
2024-05-28 13:48:58 +0200

    Cors

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ const PORT = 8000
 const express = require('express')
 const app = express()
 const axios = require('axios')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
-const url = "https://22a8e260-1c21-4d00-a294-1e3a98d82d95-europe-west2.apps.astra.datastax.com/api/rest/v2/namespaces/database_section/collections/users"
-const token = "AstraCS:OXScQTBdFpTrNbRMvPHRCyPO:c05aa1f91bdaf3b2594724d586d36a497101ce85f20ce5d2a07d8b3c912b7a56"
 
 app.post('/users', async (req, res) => {
     console.log(req)
+    const {firstName, lastName} = req.body
     const options = {
         method: "POST",
         headers: {
