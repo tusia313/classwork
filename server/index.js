38a8d82959757d242c11cd1b278d4b642fbc5702
Marta Łoskot <loskotmarta@gmail.com>
2024-06-05 17:45:10 +0200

    Wow for me

--- server/index.js
+++ server/index.js
@@ -3,9 +3,14 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 const cors = require('cors')
+const env = require('dotenv').config()
 
 //middleware
 app.use(cors())
+
+const url = "https://22a8e260-1c21-4d00-a294-1e3a98d82d95-europe-west2.apps.astra.datastax.com/api/rest/v2/namespaces/database_section/collections/users"
+const token = process.env.ASTRA_TOKEN
+console.log(token)
 
 app.delete("/users/:documentID", async (req,res) => {
     const id = req.params.documentID
