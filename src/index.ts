84f605e6b8b16963e3fd7b295b2422256f2a63ca
Marta Łoskot <loskotmarta@gmail.com>
2026-06-18 10:53:15 +0200

    Add 4 exercise

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-//Zadanie 2: Suma parzystych. Funkcja przyjmująca tablicę liczb, wybierająca tylko parzyste i zwracająca ich sumę.
-const sumEvenNumbers = (numbers) => {
- return numbers
-    .filter((number) => number % 2 === 0)
-    .reduce((sum, number) => sum + number, 0)
-};
+//Zadanie 4: Licznik samogłosek (Vowel Count)Napisz funkcję countVowels(str), która przyjmuje ciąg znaków (np. pojedyncze słowo lub całe zdanie) i zwraca liczbę samogłosek (a, e, i, o, u), które w nim występują. Wielkość liter nie powinna mieć znaczenia.
+//Przykład: countVowels("Inzynieria") powinno zwrócić 5.
+
+//Podpowiedź: Masz tu dwie drogi. Albo rozbijasz string na tablicę liter za pomocą .split('') i używasz .filter()
+
+const countVolwes = (str) => {
+    const volwes = ['a', 'e', 'i', 'o', 'u'];
+    const lowerCaseStr = str.toLowerCase()
+    const count = lowerCaseStr.split('').filter(letter => volwes.includes(letter).length)
+    return count.length
+}
