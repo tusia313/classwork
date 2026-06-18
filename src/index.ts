848ad59972d986d13767046749ca59d1ac1c6a3e
Marta Łoskot <loskotmarta@gmail.com>
2026-06-18 10:40:15 +0200

    Add exercise nr 3

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-//Napisz funkcję, która przyjmuje ciąg znaków (np. zdanie) i zwraca najdłuższe słowo z tego zdania. Zignoruj znaki interpunkcyjne.
-//Podpowiedź: Przydadzą się metody .replace(), .split() i .sort().
-
-const findLongestWord = (sentence) => {
-    // usuwanie znaków interpunkcyjnych
-    const cleanedSentence = sentence.replace(, /[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
-    // dzielenie zdania na słowa
-    const words = cleanedSentence.split(' ');
-    //sortowanie ( i to jest tablica od najdłuższego do najkrótszego)
-    const sortedWords = words.sort((a,b) => b.length - a.length)
-    // zwracanie najdłuższego słowa
-    return sortedWords[0]
-    // a najkrótszego
-    return sortedWords[sortedWords.length - 1]
-    // a z reduce tablica.reduce(funkcjaStrzalkowa, wartoscPoczatkowa) to by było tak
-     return words.reduce((longest, current) = > {
-        return current.length > longest.length ? current : longest
-     }, '')
+//Zadanie 2: Suma parzystych. Funkcja przyjmująca tablicę liczb, wybierająca tylko parzyste i zwracająca ich sumę.
+const sumEvenNumbers = (numbers) => {
+ return numbers
+    .filter((number) => number % 2 === 0)
+    .reduce((sum, number) => sum + number, 0)
+};
