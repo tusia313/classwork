1c886b17d6d8f20c61b63f3297e381c327f8a6b5
Marta Łoskot <loskotmarta@gmail.com>
2026-06-23 14:52:28 +0200

    generics practice

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-//Zadanie 4: Licznik samogłosek (Vowel Count)Napisz funkcję countVowels(str), która przyjmuje ciąg znaków (np. pojedyncze słowo lub całe zdanie) i zwraca liczbę samogłosek (a, e, i, o, u), które w nim występują. Wielkość liter nie powinna mieć znaczenia.
-//Przykład: countVowels("Inzynieria") powinno zwrócić 5.
-
-//Podpowiedź: Masz tu dwie drogi. Albo rozbijasz string na tablicę liter za pomocą .split('') i używasz .filter()
-
-const countVolwes = (str) => {
-    const volwes = ['a', 'e', 'i', 'o', 'u'];
-    const lowerCaseStr = str.toLowerCase()
-    const count = lowerCaseStr.split('').filter(letter => volwes.includes(letter).length)
-    return count.length
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Change these functions into generic functions by altering the
+// type signatures. There should be no `unknown` types when you are done
+function randomFromList<T>(list: T[]): T {
+  const length = list.length;
+  const index = Math.floor(Math.random() * length);
+  return list[index];
 }
+function duplicateList<T>(list: T[], count: number = 1) {
+  let output: T[] = [];
+  for (let i = 0; i < count; i++) {
+    output = output.concat(list);
+  }
+  return output;
+}
+function createTuple<T, K>(item1: T, item2: K) {
+  return [item1, item2];
+}
+
+// Use the following interface to constrain the generic in the next function
+interface Length {
+  length: number;
+}
+function getLength<T extends Length>(item: T): number {
+  return item.length;
+}
+
+
+
